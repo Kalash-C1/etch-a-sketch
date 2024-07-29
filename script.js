367d1fc777435657b314c4bccb7e32c77abf3d4f
Kalash-C1 <chetiwalkalash@gmail.com>
2024-07-29 13:40:57 +0530

    Implemented the hover effect

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ const container = document.querySelector(".container");
 for(let i = 0; i < 16; i++) {
     container.appendChild(divs[i]);
 }
+
+const sq = document.querySelectorAll(".square");
+sq.forEach(sq => {
+    sq.addEventListener("mouseenter", function(e) {
+        e.target.style.backgroundColor = "red";
+    });
+    sq.addEventListener("mouseleave" , function(e) {
+        e.target.style.backgroundColor = "white";
+    });
+
+});
