d432d6b234653cc72c974dd6c68d0f0974a8d290
Kalash-C1 <chetiwalkalash@gmail.com>
2024-07-30 09:32:02 +0530

    added the functionality to randomize colors

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-const divs =[];
-for(let i = 0; i < 16; i++) {
-    const div = document.createElement("div");
-    div.classList.add("square");
-    divs[i] = div; 
-    
+let ip = prompt("Enter the number for which u want a grid: ");
+if(ip > 100) {
+    alert("Maximum value is 100");
 }
-const container = document.querySelector(".container");
-for(let i = 0; i < 16; i++) {
-    container.appendChild(divs[i]);
+else {
+    const divs =[];
+    for(let i = 0; i < ip*ip; i++) {
+        const div = document.createElement("div");
+        div.classList.add("square");
+        divs[i] = div; 
+        
+    }
+    const container = document.querySelector(".container");
+    for(let i = 0; i < ip*ip; i++) {
+        container.appendChild(divs[i]);
+    }
+    function randomInt() {
+        return Math.floor(Math.random()*255);
+    }
+    const sq = document.querySelectorAll(".square");
+    sq.forEach(sq => {
+        sq.addEventListener("mouseenter", function(e) {
+            const r = randomInt();
+            const g = randomInt();
+            const b = randomInt();
+            e.target.style.backgroundColor = `rgb(${r}, ${r}, ${r})`;
+            // e.target.style.backgroundColor = "blue";
+        });
+    });
+    sq.forEach(sq => {
+        sq.style.width = (100/ip) + "%";
+        sq.style.height = (100/ip) + "%";
+    });
 }
-
-const sq = document.querySelectorAll(".square");
-sq.forEach(sq => {
-    sq.addEventListener("mouseenter", function(e) {
-        e.target.style.backgroundColor = "red";
-    });
-    sq.addEventListener("mouseleave" , function(e) {
-        e.target.style.backgroundColor = "white";
-    });
-
-});
